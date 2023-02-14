97d96a8951dba7bc12f99c9a7f8751bff3c372c2
안효웅 <skow951216@gmail.com>
2023-02-14 09:59:27 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package chap01.exam01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+				
+		System.out.println("Hello, Java");
+		System.out.println("\\\\\\");
+		
+	}
+
+}
